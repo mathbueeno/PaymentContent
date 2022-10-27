ab567b0b10b8e6551132b18b9309577df81c8940
mathbueeno <mathbueeno@gmail.com>
2022-10-27 10:13:20 -0300

    Update dos Value Objects

--- PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -9,7 +9,7 @@ namespace PaymentContext.Domain.Entities
     // Abstract - significa que ela fica em abstrato, tendo que ser relacionada o qualquer outro tipo abaixo, por exemplo : Boleto
     public abstract class Payment
     {
-        protected Payment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string owner, Document document, Address address, Email email)
+        protected Payment(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string Payer, Document document, Address address, Email email)
         {
             //1- Gerou novo Guid, 2- Converteu para String, 3- Removeu o traço, 4- pegou 10 primeiros caracteres
             // 5- Deixou maiúsculo
@@ -19,7 +19,7 @@ namespace PaymentContext.Domain.Entities
             ExpireDate = expireDate;
             Total = total;
             TotalPaid = totalPaid;
-            Owner = owner;
+            Payer = Payer;
             Document = document;
             Address = address;
             Email = email;
@@ -30,7 +30,7 @@ namespace PaymentContext.Domain.Entities
         public DateTime ExpireDate { get; private set; }
         public decimal Total { get; private set; }
         public decimal TotalPaid { get; private set; }
-        public string Owner { get; private set; }
+        public string Payer { get; private set; }
         public Document Document { get; private set; }
         public Address Address { get; private set; }
         public Email Email { get; private set; }
